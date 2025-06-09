04a65c1eeb82efce293319e54881ba9b03aaa171
Giovani <giovani.guedes@outlook.com>
2025-06-08 23:09:17 -0300

    Update grupoTeste.js

--- grupoTeste.js
+++ grupoTeste.js
@@ -17,7 +17,7 @@
 
     const html = `
         <div class="vis" style="padding: 10px; width: 800px;">
-            <h2>Grupos de Aldeias versão 1.3</h2>
+            <h2>Grupos de Aldeias versão 1.5</h2>
             <label for="groupSelect"><b>Selecione um grupo:</b></label><br>
             <select id="groupSelect" style="
                 margin-top: 5px;
