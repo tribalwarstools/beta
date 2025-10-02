fa601b87e13624f14f33c3deac0aae4dbe6701f8
Giovani <giovani.guedes@outlook.com>
2025-10-02 08:19:36 -0300

    Update NotasReatorio.js

--- NotasReatorio.js
+++ NotasReatorio.js
@@ -1,142 +1,170 @@
-var DadosScript = {
-    nome: "Notas automáticas de relatórios",
-    versao: "",
-    ultimaAtualizacao: "",
-    autor: "",
-    contatoAutor: ""
-};
+// ==UserScript==
+// @name         Notas Automáticas de Relatórios TW
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Cria notas automáticas de relatórios, detectando tipo de aldeia
+// @author       Giovani Guedes
+// @match        *://*.tribalwars.com.br/game.php*screen=report*
+// @grant        none
+// ==/UserScript==
 
-var PREFIXO_LS = "";
+(function() {
+    'use strict';
 
-// Traduções apenas em português brasileiro
-var traducoes = {
-    pt_BR: {
-        desconhecido: "Desconhecido",
-        verificarPagina: "O script deve ser utilizado dentro de um relatório",
-        ofensiva: "Ofensiva",
-        defensiva: "Defensiva",
-        provavelmenteOfensiva: "Provavelmente Ofensiva",
-        provavelmenteDefensiva: "Provavelmente Defensiva",
-        nenhumaSobreviveu: "Nenhuma tropa sobreviveu",
-        torre: "Torre de Vigia",
-        muralha: "Muralha",
-        igrejaPrincipal: "Igreja Principal",
-        igreja: "Igreja",
-        fullsDefesa: "Fulls de Defesa",
-        notaCriada: "Nota criada",
-        adicionarRelatorio: "Adicionar relatório na:"
-    }
-};
+    var LS_prefix = "xd";
 
-// Função de tradução
-var _t = chave => traducoes.pt_BR[chave];
+    // Traduções
+    var translations = {
+        pt_BR: {
+            unknown: "Desconhecido",
+            verifyReportPage: "O script deve ser utilizado dentro de um relatório",
+            offensive: "Ofensiva",
+            defensive: "Defensiva",
+            probOffensive: "Provavelmente Ofensiva",
+            probDefensive: "Provavelmente Defensiva",
+            noSurvivors: "Nenhuma tropa sobreviveu",
+            watchtower: "Torre de Vigia",
+            wall: "Muralha",
+            firstChurch: "Igreja Principal",
+            church: "Igreja",
+            defensiveNukes: "Fulls de Defesa",
+            noteCreated: "Nota criada"
+        }
+    };
 
-// Inicialização de tradução
-var inicializarTraducoes = () => {
-    if (!localStorage.getItem(`${PREFIXO_LS}_avisoIdioma`)) {
-        localStorage.setItem(`${PREFIXO_LS}_avisoIdioma`, 1);
-    }
-};
+    var _t = chave => translations.pt_BR[chave];
 
-// Script principal
-var CriarNotasRelatorio = {
-    dados: {
-        jogador: { nome: game_data.player.name, estaAtacando: false, estaDefendendo: false, querInfoAtacante: false, querInfoDefensor: false },
-        aldeia: {
-            ofensiva: { id: "-1", tipo: _t("desconhecido"), tropas: { totais: [], ofensivas: 0, defensivas: 0 } },
-            defensiva: { 
-                id: "-1", 
-                tipo: _t("desconhecido"), 
-                tropas: { visiveis: false, totais: [], fora: { visiveis: false, ofensivas: 0, defensivas: 0, totais: [] }, dentro: { ofensivas: 0, defensivas: 0, totais: [] }, apoios: 0 },
-                edificios: { visiveis: false, torre: [false,0], igrejaPrincipal: [false,0], igreja: [false,0], muralha: [false,0] }
-            }
+    // Script principal
+    var CriarNotasRelatorio = {
+        dados: {
+            player: {
+                nomePlayer: game_data.player.name,
+                playerEstaAtacar: false,
+                playerEstaDefender: false,
+                playerQuerInfoAtacante: false,
+                playerQuerInfoDefensor: false
+            },
+            aldeia: {
+                ofensiva: { idAldeia: "-1", tipoAldeia: _t("unknown"), tropas: { totais: [], ofensivas: 0, defensivas: 0 } },
+                defensiva: {
+                    idAldeia: "-1",
+                    tipoAldeia: _t("unknown"),
+                    tropas: {
+                        visiveis: false,
+                        totais: [],
+                        fora: { visiveis: false, ofensivas: 0, defensivas: 0, totais: [] },
+                        dentro: { ofensivas: 0, defensivas: 0, totais: [] },
+                        apoios: 0
+                    },
+                    edificios: { visiveis: false, torre: [false,0], igrejaPrincipal: [false,0], igreja: [false,0], muralha: [false,0] }
+                }
+            },
+            mundo: { fazendaPorTropa: [], arqueirosAtivos: false }
         },
-        mundo: { fazendaPorTropa: [], arqueirosAtivos: false }
-    },
-    configs: { esconderTropas: false },
 
-    verificarPagina: function() {
-        var urlValida = window.location.href.match(/(screen\=report){1}|(view\=){1}\w+/g);
-        if (!urlValida || urlValida.length != 2) {
-            UI.ErrorMessage(_t("verificarPagina"), 5000);
-            return false;
+        verificarPagina: function() {
+            var urlValida = window.location.href.match(/(screen\=report){1}|(view\=){1}\w+/g);
+            if (!urlValida || urlValida.length != 2) {
+                UI.ErrorMessage(_t("verifyReportPage"), 5000);
+                return false;
+            }
+            return true;
+        },
+
+        initDadosScript: function() {
+            var self = this;
+            this.dados.mundo.arqueirosAtivos = game_data.units.includes("archer");
+            var len = this.dados.mundo.arqueirosAtivos ? 10 : 8;
+
+            this.dados.aldeia.ofensiva.tropas.totais = new Array(len).fill(0);
+            this.dados.aldeia.defensiva.tropas.totais = new Array(len).fill(0);
+            this.dados.aldeia.defensiva.tropas.fora.totais = new Array(len).fill(0);
+            this.dados.aldeia.defensiva.tropas.dentro.totais = new Array(len).fill(0);
+
+            this.dados.mundo.fazendaPorTropa = this.dados.mundo.arqueirosAtivos
+                ? [1,1,1,1,2,4,5,6,5,8]
+                : [1,1,1,2,4,6,5,8];
+
+            // Detecta atacante e defensor
+            var atacante = $("#attack_info_att > tbody > tr:nth-child(1) > th:nth-child(2) > a").text(),
+                defensor = $("#attack_info_def > tbody > tr:nth-child(1) > th:nth-child(2) > a").text(),
+                pos = game_data.player.sitter != "0" ? 4 : 3;
+
+            self.dados.aldeia.ofensiva.idAldeia = $("#attack_info_att > tbody > tr:nth-child(2) > td:nth-child(2) > span > a:nth-child(1)").url().split("=")[pos];
+            self.dados.aldeia.defensiva.idAldeia = $("#attack_info_def > tbody > tr:nth-child(2) > td:nth-child(2) > span > a:nth-child(1)").url().split("=")[pos];
+
+            if (defensor == this.dados.player.nomePlayer) this.dados.player.playerEstaDefender = true;
+            if (atacante == this.dados.player.nomePlayer) this.dados.player.playerEstaAtacar = true;
+
+            // Verifica tropas visíveis defensivas
+            if($("#attack_info_def_units > tbody > tr:nth-child(2) > td").length) this.dados.aldeia.defensiva.tropas.visiveis = true;
+        },
+
+        getTipoAldeia: function() {
+            var def = this.dados.aldeia.defensiva.tropas;
+            var ofs = this.dados.aldeia.ofensiva.tropas;
+
+            // Defensiva
+            if(def.visiveis){
+                if(def.dentro.ofensivas > 3000) this.dados.aldeia.defensiva.tipoAldeia = _t("offensive");
+                else if(def.dentro.ofensivas > 500) this.dados.aldeia.defensiva.tipoAldeia = _t("probOffensive");
+                else if(def.dentro.defensivas > 1000) this.dados.aldeia.defensiva.tipoAldeia = _t("defensive");
+                else if(def.dentro.defensivas > 500) this.dados.aldeia.defensiva.tipoAldeia = _t("probDefensive");
+                else this.dados.aldeia.defensiva.tipoAldeia = _t("noSurvivors");
+            } else {
+                this.dados.aldeia.defensiva.tipoAldeia = _t("noSurvivors");
+            }
+
+            // Ofensiva
+            if(ofs.ofensivas > ofs.defensivas) this.dados.aldeia.ofensiva.tipoAldeia = _t("offensive");
+            else if(ofs.ofensivas < ofs.defensivas) this.dados.aldeia.ofensiva.tipoAldeia = _t("defensive");
+        },
+
+        gerarTextoNota: function() {
+            var nota = "";
+            var textoRelatorio = $("#report_export_code").text();
+            var titulo = $("#content_value > table > tbody > tr > td:nth-child(2) > table > tbody > tr > td > table:nth-child(2) > tbody > tr:nth-child(2)").text()
+                .replace(/\s+/g, " ")
+                .replace(/.{5}$/g, "");
+
+            var tipoAldeia = this.dados.player.playerEstaAtacar || this.dados.player.playerQuerInfoDefensor
+                ? this.dados.aldeia.defensiva.tipoAldeia
+                : this.dados.aldeia.ofensiva.tipoAldeia;
+
+            nota += " | [color=#" + (tipoAldeia == _t("offensive") || tipoAldeia == _t("probOffensive") ? "ff0000" : "0eae0e") + "][b]" + tipoAldeia + "[/b][/color] | ";
+            
+            var agora = new Date();
+            nota += "[b][size=6]" + agora.toLocaleString() + "[/size][/b]\n\n[b]" + titulo + "[/b]";
+
+            nota += textoRelatorio;
+
+            return nota;
+        },
+
+        escreverNota: function() {
+            var idAldeia = this.dados.player.playerEstaAtacar || this.dados.player.playerQuerInfoDefensor
+                ? parseInt(this.dados.aldeia.defensiva.idAldeia)
+                : parseInt(this.dados.aldeia.ofensiva.idAldeia);
+
+            var urlAPI = game_data.player.sitter == "0"
+                ? `https://${location.hostname}/game.php?village=${game_data.village.id}&screen=api&ajaxaction=village_note_edit&h=${game_data.csrf}&client_time=${Math.round(Timing.getCurrentServerTime()/1000)}`
+                : `https://${location.hostname}/game.php?village=${game_data.village.id}&screen=api&ajaxaction=village_note_edit&t=${game_data.player.id}`;
+
+            $.post(urlAPI, { note: this.gerarTextoNota(), village_id: idAldeia, h: game_data.csrf }, function() {
+                UI.SuccessMessage(_t("noteCreated"), 2000);
+            });
+        },
+
+        start: function() {
+            if(this.verificarPagina()){
+                this.initDadosScript();
+                this.getTipoAldeia();
+                this.escreverNota();
+            }
         }
-        return true;
-    },
+    };
 
-    initConfigs: function() {
-        this.configs.esconderTropas = this.loadConfigLS("esconderTropas", false);
-    },
+    // Executa automaticamente
+    CriarNotasRelatorio.start();
 
-    loadConfigLS: (chave, valorPadrao) => localStorage.getItem(`${PREFIXO_LS}_${chave}`) ?? valorPadrao,
-
-    iniciarDadosScript: function() {
-        var self = this;
-        this.dados.mundo.arqueirosAtivos = game_data.units.includes("archer");
-
-        var len = this.dados.mundo.arqueirosAtivos ? 10 : 8;
-        this.dados.aldeia.ofensiva.tropas.totais = new Array(len).fill(0);
-        this.dados.aldeia.defensiva.tropas.totais = new Array(len).fill(0);
-        this.dados.aldeia.defensiva.tropas.fora.totais = new Array(len).fill(0);
-        this.dados.aldeia.defensiva.tropas.dentro.totais = new Array(len).fill(0);
-
-        this.dados.mundo.fazendaPorTropa = this.dados.mundo.arqueirosAtivos
-            ? [1,1,1,1,2,4,5,6,5,8]
-            : [1,1,1,2,4,6,5,8];
-
-        // Detecta atacante e defensor
-        var atacante = $("#attack_info_att > tbody > tr:nth-child(1) > th:nth-child(2) > a").text(),
-            defensor = $("#attack_info_def > tbody > tr:nth-child(1) > th:nth-child(2) > a").text(),
-            pos = game_data.player.sitter != "0" ? 4 : 3;
-
-        self.dados.aldeia.ofensiva.id = $("#attack_info_att > tbody > tr:nth-child(2) > td:nth-child(2) > span > a:nth-child(1)").url().split("=")[pos];
-        self.dados.aldeia.defensiva.id = $("#attack_info_def > tbody > tr:nth-child(2) > td:nth-child(2) > span > a:nth-child(1)").url().split("=")[pos];
-
-        if (defensor == this.dados.jogador.nome) this.dados.jogador.estaDefendendo = true;
-        if (atacante == this.dados.jogador.nome) this.dados.jogador.estaAtacando = true;
-    },
-
-    gerarTextoNota: function() {
-        var nota = "",
-            textoRelatorio = $("#report_export_code").text(),
-            titulo = $("#content_value > table > tbody > tr > td:nth-child(2) > table > tbody > tr > td > table:nth-child(2) > tbody > tr:nth-child(2)").text().replace(/\s+/g, " ").replace(/.{5}$/g, "");
-
-        var tipoAldeia = this.dados.jogador.estaAtacando || this.dados.jogador.querInfoDefensor
-            ? this.dados.aldeia.defensiva.tipo
-            : this.dados.aldeia.ofensiva.tipo;
-
-        nota += " | [color=#" + (tipoAldeia == _t("ofensiva") || tipoAldeia == _t("provavelmenteOfensiva") ? "ff0000" : "0eae0e") + "][b]" + tipoAldeia + "[/b][/color] | ";
-        var agora = new Date();
-var dataHora = agora.toLocaleString();
-nota += "[b][size=6]" + dataHora + "[/size][/b]\n\n[b]" + titulo + "[/b]";
-
-        nota += textoRelatorio;
-
-        return nota;
-    },
-
-    escreverNota: function() {
-        var idAldeia = this.dados.jogador.estaAtacando || this.dados.jogador.querInfoDefensor
-            ? parseInt(this.dados.aldeia.defensiva.id)
-            : parseInt(this.dados.aldeia.ofensiva.id);
-
-        var urlAPI = game_data.player.sitter == "0"
-            ? `https://${location.hostname}/game.php?village=${game_data.village.id}&screen=api&ajaxaction=village_note_edit&h=${game_data.csrf}&client_time=${Math.round(Timing.getCurrentServerTime()/1000)}`
-            : `https://${location.hostname}/game.php?village=${game_data.village.id}&screen=api&ajaxaction=village_note_edit&t=${game_data.player.id}`;
-
-        $.post(urlAPI, { note: this.gerarTextoNota(), village_id: idAldeia, h: game_data.csrf }, function() {
-            UI.SuccessMessage(_t("notaCriada"), 2000);
-        });
-    },
-
-    start: function() {
-        if (this.verificarPagina()) {
-            this.iniciarDadosScript();
-            this.escreverNota();
-        }
-    }
-};
-
-// Inicializa e executa
-inicializarTraducoes();
-CriarNotasRelatorio.start();
+})();
