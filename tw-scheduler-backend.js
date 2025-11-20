c84afa7a53652dc440d38a562a62866881d5aaee
Giovani <giovani.guedes@outlook.com>
2025-11-20 16:11:49 -0300

    Update tw-scheduler-backend.js

--- tw-scheduler-backend.js
+++ tw-scheduler-backend.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  // === Configs / Constantes ===
+  // === Configs /Constantes ===
   const STORAGE_KEY = 'tw_scheduler_multi_v1';
   const PANEL_STATE_KEY = 'tws_panel_state';
   const TROOP_LIST = ['spear','sword','axe','archer','spy','light','marcher','heavy','ram','catapult','knight','snob'];
@@ -634,3 +634,4 @@
 
   console.log('[TWS_Backend] Backend carregado com sucesso (v2.5 - Validação Pós-Execução)');
 })();
+
